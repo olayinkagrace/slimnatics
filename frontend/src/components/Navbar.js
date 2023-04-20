dd71f570795cfd34e4119ec44d7111376c4f8f06
Olayinka <olayinka4grace@gmail.com>
2023-04-20 20:46:07 +0100

    slimnatics

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -16,7 +16,7 @@ function Navbar() {
     <header>
       <div className='container'>
         <Link to='/'>
-          <h1>Workout Buddy</h1>
+          <h1>Slimnatics</h1>
         </Link>
         <nav>
           {user && (
