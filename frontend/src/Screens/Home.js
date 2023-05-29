1d8f4ea31e1f57c6c12a587cf1d3e1fd0c8d3406
Olayinka <olayinka4grace@gmail.com>
2023-05-29 15:58:58 +0100

    commit

--- frontend/src/Screens/Home.js
+++ frontend/src/Screens/Home.js
@@ -14,7 +14,7 @@ function Home() {
  useEffect(() => {
      const fetchWorkout = async () => {
       
-         const response = await fetch('http://localhost:4000/api/workouts', {
+         const response = await fetch('https://slimm.onrender.com/api/workouts', {
             headers: {
                 'Authorization': `Bearer ${user.token}`
             }
